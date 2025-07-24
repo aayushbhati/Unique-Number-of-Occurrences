539bf822cf6b39f4657c7020666dd1afbb1130f5
aayushbhati <124025774+aayushbhati@users.noreply.github.com>
2025-07-24 21:33:57 +0530

    Day21.cpp

---
+++ Day21.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<arr.size();i++) mp[arr[i]]++;
+
+        unordered_set<int> freq;
+        for(auto i: mp){
+            if(freq.find(i.second) != freq.end()) return false;
+            freq.insert(i.second);
+        }
+        return true;
+    }
+};
